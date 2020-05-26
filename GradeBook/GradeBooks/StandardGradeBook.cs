44834cf798bda790ef91739b8950d8a43b94cf19
Jamesbond707 <51248173+jaimecruz707@users.noreply.github.com>
2020-05-26 16:06:56 -0400

    Completed module

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,16 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook( string name ) : base( name )
+        {
+            Type = GradeBookType.Standard; // initializes the the type of GradeBook to be a standard Type
+
+        }
+    }
+}
