45cec37f105cf720c323dd3e10ebc06331377660
Jamesbond707 <51248173+jaimecruz707@users.noreply.github.com>
2020-05-26 16:44:48 -0400

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -11,5 +11,48 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade( double averageGrade )
+        {
+            if ( Students.Count < 5 )
+            {
+                throw new InvalidOperationException();
+            }
+
+            var allAverageGrade = new List<double>();
+
+            Students.ForEach( n => allAverageGrade.Add( n.AverageGrade ) );
+
+            allAverageGrade.Sort();
+            allAverageGrade.Reverse();
+            var sizes = new List<double>()
+            {
+                allAverageGrade.Count * 0.2,
+                (allAverageGrade.Count * 0.4),
+                allAverageGrade.Count * 0.6,
+                allAverageGrade.Count * 0.8
+            };
+
+            for ( int i = 0, position = 0; i < allAverageGrade.Count; i++, position++ )
+            {
+                if( allAverageGrade[i] < averageGrade )
+                {
+                    position = i;
+                    if ( position >= sizes[0] )
+                        return 'A';
+                    else if ( sizes[0] < position && position < sizes[1] )
+                        return 'B';
+                    else if ( sizes[1] < position && position < sizes[2] )
+                        return 'C';
+                    else if ( sizes[2] < position && position < sizes[3] )
+                        return 'D';
+                    break;
+                }
+            }
+
+            return 'F';
+        }
+
+
     }
 }
